0db071cff4e951b62915adc630898ea532f54e7c
Pavan2004-b <127078470+Pavan2004-b@users.noreply.github.com>
2024-12-01 23:48:18 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -44,7 +44,7 @@ const PlaceOrder = () => {
     let response = await axios.post(url+'/api/order/place', orderData,{headers:{token}})
     if(response.data.success){
       const {session_url} = response.data;
-      window.location.replace(session_url);
+      window.location.replace();
     }
     else{
       alert('Error')
